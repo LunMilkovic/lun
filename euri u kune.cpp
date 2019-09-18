c60f94c7c1cd2f0cc03592b6c2323006fae9b0e5
LunMilkovic <55487387+LunMilkovic@users.noreply.github.com>
2019-09-18 12:37:28 +0200

    Add files via upload

---
+++ euri u kune.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"upisi eure";
+	cin>>a;
+	b=7.4;
+	c=a*b;
+	cout<<"kune:"<<c<<endl;
+
+
+}
+
+
+
+
+
