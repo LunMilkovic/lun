1149ce8b2524713ee772dcb4c95f30213884ae7d
LunMilkovic <55487387+LunMilkovic@users.noreply.github.com>
2019-11-04 14:38:59 +0100

    Add files via upload

---
+++ JJJ.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char pitanje[]="Upisi svoje ime:";
+	char pozdrav[]="Pozdrav,";
+	char ime[80];
+	cout<<pitanje;
+	cin>>ime;
+	cout<<"pozdrav "<<ime<<"!";
+	
+	
+}
---
+++ kk.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int a[11]={racunalstvo};
+	int i,n=6,;
+	for(i=0;i<n;++i);
+	cout<<i<<;
+}
